a306b30629a0137187c43d941ba1504753e6e8dc
Nityy20 <vincent-2905@hotmail.Fr>
2025-04-22 21:18:56 +0200

    Correction collapse

--- kasa/src/components/Slideshow.jsx
+++ kasa/src/components/Slideshow.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Slideshow({ pictures }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -26,15 +31,17 @@ function Slideshow({ pictures }) {
             className="slideshow__arrow slideshow__arrow--left"
             onClick={goToPrevious}
           >
-            ‹
+            <FontAwesomeIcon icon={faChevronLeft} />
           </button>
+
           <button
             className="slideshow__arrow slideshow__arrow--right"
             onClick={goToNext}
           >
-            ›
+            <FontAwesomeIcon icon={faChevronRight} />
           </button>
-          <div className="slideshow__counter">
+
+          <div className="slideshow__counter slideshow__number">
             {currentIndex + 1} / {total}
           </div>
         </>
